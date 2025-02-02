0493c758789bd0137cc851b8bf99bdbf7d245c4b
byjins <ppwm111@naver.com>
2025-02-02 17:20:43 +0900

    /blog/[category] params 비동기 객체 수정

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { getPostsByCategory } from "@/lib/posts";
 import PostCard from "@/components/ui/PostCard";
 
+interface Props {
+  params: Promise<{ category: string }>;
+}
 // 카테고리 뱃지 클릭 시 해당 카테고리 페이지만 나와야함.
-const CategoryPage = ({ params }: { params: { category: string } }) => {
-  const filteredContent = getPostsByCategory(params.category);
+const CategoryPage = async ({ params }: Props) => {
+  const category = (await params).category;
+
+  const filteredContent = getPostsByCategory(category);
 
   return (
     <article className={"flex flex-col gap-6"}>
