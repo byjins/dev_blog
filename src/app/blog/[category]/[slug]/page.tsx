ede788c0ca07cd0b6f539bbeccf54b1335e2c0f3
byjins <ppwm111@naver.com>
2025-02-02 17:18:56 +0900

    params 비동기 객체 수정

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+interface Props {
+  params: Promise<{ slug: string }>;
+}
+
 // 선택한 게시글의 디테일 페이지만 보여아함.
-const DetailPage = ({ params }: { params: { slug: string } }) => {
-  console.log(params.slug);
-  return <div></div>;
+const DetailPage = async ({ params }: Props) => {
+  const slug = (await params).slug;
+
+  return <div>{slug}</div>;
 };
 
 export default DetailPage;
